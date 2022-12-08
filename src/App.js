b59352cbd990051beb6dd3aaf93d254db301693c
Blake Stefansen <109697058+BStefansen-lightbox@users.noreply.github.com>
2022-12-08 15:54:58 -0500

    stuff

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
+import * as d3 from "d3";
 import logo from './logo.svg';
 import './App.css';
+import Map from './Map.js';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+	  <div id="main">
+	    <div id="title">United States Educational Attainment</div>
+	    <div id="description">Percentage of adults age 25 and older with a bachelor's degree or higher (2010-2014)</div>
+	    <div id="data"></div>
+	  </div>
+	  
+	  <Map />
+	  
+	  <div id="footer">
+	    <div id="source">Source: <a href='https://www.ers.usda.gov/data-products/county-level-data-sets/download-data.aspx' target="_blank">USDA Economic Research Service</a></div>
+	    <a href="https://bstefansen.github.io/Portfolio/" target="_blank">by Blake Stefansen</a>
+	  </div>
       </header>
     </div>
   );
