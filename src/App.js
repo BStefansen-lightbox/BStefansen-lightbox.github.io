5f0a17f7ebc19e80f6626957253ae334a9d3b1a4
Blake Stefansen <109697058+BStefansen-lightbox@users.noreply.github.com>
2022-12-08 17:18:54 -0500

    stuff

--- src/App.js
+++ src/App.js
@@ -11,15 +11,15 @@ function App() {
 	  <div id="main">
 	    <div id="title">United States Educational Attainment</div>
 	    <div id="description">Percentage of adults age 25 and older with a bachelor's degree or higher (2010-2014)</div>
-	    <div id="data"></div>
+	    <div id="data" class="flex items-center justify-center">
+		<Map />
+	    </div>
 	  </div>
-	  
-	  <Map />
-	  
-	  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+	
+	  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded-full">
 	    <a href='https://www.ers.usda.gov/data-products/county-level-data-sets/download-data.aspx' target="_blank">USDA Economic Research Service</a>
 	  </button>
-	  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+	  <button class="bg-blue-500 mt-6 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
 	    <a href="https://github.com/bstefansen" target="_blank">Site Author</a>
 	  </button>
       </header>
