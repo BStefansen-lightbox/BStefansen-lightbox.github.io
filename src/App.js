e034668d01a2d85ce36577dc155b51262b8bfa93
Blake Stefansen <109697058+BStefansen-lightbox@users.noreply.github.com>
2022-12-13 09:25:32 -0500

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import * as d3 from "d3";
-import logo from './logo.svg';
 import './App.css';
 import Map from './Map.js';
 
@@ -17,10 +15,10 @@ function App() {
 	  </div>
 	
 	  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded-full">
-	    <a href='https://www.ers.usda.gov/data-products/county-level-data-sets/download-data.aspx' target="_blank">USDA Economic Research Service</a>
+	    <a href='https://www.ers.usda.gov/data-products/county-level-data-sets/download-data.aspx' target="_blank" rel="noreferrer">USDA Economic Research Service</a>
 	  </button>
 	  <button class="bg-blue-500 mt-6 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-	    <a href="https://github.com/bstefansen" target="_blank">Site Author</a>
+	    <a href="https://github.com/bstefansen" target="_blank" rel="noreferrer">Site Author</a>
 	  </button>
       </header>
     </div>
